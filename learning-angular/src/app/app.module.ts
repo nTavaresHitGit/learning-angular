e90437d64c1a2d694aad6a581d3e49fd61d7a35e
nTavaresHitGit <56731932+nTavaresHitGit@users.noreply.github.com>
2020-12-02 21:48:44 -0500

    Assignment8
    Assignment 8, quite buggy. Followed tutorial and continue to get bugs.

--- learning-angular/src/app/app.module.ts
+++ learning-angular/src/app/app.module.ts
@@ -10,7 +10,13 @@ import { MessagesComponent } from './messages/messages.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './services/in-memory-data.service';
-import { CreateComponentComponent } from './create-component/create-component.component';
+import {AddContentDialog, CreateComponentComponent} from './create-component/create-component.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRippleModule} from '@angular/material/core';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
@@ -21,6 +27,7 @@ import { CreateComponentComponent } from './create-component/create-component.co
     HoverStyleDirective,
     MessagesComponent,
     CreateComponentComponent,
+    AddContentDialog
 
   ],
   imports: [
@@ -28,10 +35,19 @@ import { CreateComponentComponent } from './create-component/create-component.co
     FormsModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false,
-        delay: 1000 })
+      InMemoryDataService, {
+        dataEncapsulation: false,
+        delay: 1000
+      }),
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRippleModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AddContentDialog]
 })
 export class AppModule { }
--- learning-angular/src/app/create-component/create-component.component.ts
+++ learning-angular/src/app/create-component/create-component.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Inject, OnInit, Output, ViewChild} from '@angular/core';
 import {Content} from '../helper-files/content-interface';
 import {ContentService} from '../services/content.service';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-create-component',
@@ -17,7 +18,7 @@ export class CreateComponentComponent implements OnInit {
   // @ViewChild('id') inputId;
   @Output() newGameEvent: EventEmitter<Content> = new EventEmitter<Content>();
   newGame: any;
-  constructor(private contentService: ContentService) {this.newGame = {
+  constructor(private contentService: ContentService, public dialog: MatDialog) {this.newGame = {
     title: '',
     author: '',
     genre: '',
@@ -28,6 +29,15 @@ export class CreateComponentComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  openAddGameDialog(): void {
+    const gameDialogRef = this.dialog.open(AddContentDialog, {
+      width: '400px'
+    });
+    gameDialogRef.afterClosed().subscribe(newGameFromDialog => {
+      this.newGame = newGameFromDialog;
+      this.addGame();
+    });
+  }
   addGame(): void {
     let newGameFromServer: Content;
     console.log('Trying to add the game', this.newGame);
@@ -36,14 +46,35 @@ export class CreateComponentComponent implements OnInit {
       console.log(serverGame);
       this.newGameEvent.emit(newGameFromServer);
     });
-    this.clearFields();
+    // this.clearFields();
   }
-  clearFields(): void{
-    this.inputName.nativeElement.value = ' ';
-    this.inputAuthor.nativeElement.value = ' ';
-    this.inputBody.nativeElement.value = ' ';
-    this.inputGenre.nativeElement.value = ' ';
-    this.inputImg.nativeElement.value = ' ';
-    // this.inputId.nativeElement.value = ' ';
+  // clearFields(): void{
+  //   this.inputName.nativeElement.value = ' ';
+  //   this.inputAuthor.nativeElement.value = ' ';
+  //   this.inputBody.nativeElement.value = ' ';
+  //   this.inputGenre.nativeElement.value = ' ';
+  //   this.inputImg.nativeElement.value = ' ';
+  //   // this.inputId.nativeElement.value = ' ';
+  // }
+}
+@Component({
+  selector: 'app-add-content-dialog',
+  templateUrl: './add-content-dialog.component.html',
+})
+export class AddContentDialog {
+  newGame: any;
+  constructor(
+    public dialogRef: MatDialogRef<AddContentDialog>) {this.newGame = {
+    title: '',
+    author: '',
+    genre: '',
+    body: '',
+    imgUrl: ''
+  };
   }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
